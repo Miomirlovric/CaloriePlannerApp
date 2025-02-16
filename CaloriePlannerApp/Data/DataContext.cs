b00991ddede6b587565167dc26f67303254695bd
Miomir <miomir.oe@gmail.com>
2025-02-16 12:08:54 +0100

    Add project files.

---
+++ CaloriePlannerApp/Data/DataContext.cs
@@ -0,0 +1,33 @@
+﻿using CaloriePlannerApp.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaloriePlannerApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=maui.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly, (Type type) =>
+            {
+                return type.Namespace.Contains("Configurations");
+            });
+        }
+    }
+}
---
+++ CaloriePlannerApp/MauiProgram.cs
@@ -0,0 +1,40 @@
+﻿using CaloriePlannerApp.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace CaloriePlannerApp;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+        builder.Services.AddDbContext<DataContext>();
+
+        var app = builder.Build();
+
+        using (var scope = app.Services.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetService<DataContext>();
+
+            if (context != null)
+            {
+                context.Database.Migrate();
+            }
+        }
+
+        return app;
+    }
+}
